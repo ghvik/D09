a5f6ac33df0397ca42f1e14bc54cc32041116e56
ghvik <vreddy@ischool.berkeley.edu>
2016-08-15 20:38:01 -0700

    Finished ch12, ex01, printing most frequent letters in string

--- HW09_ch12_ex01.py
+++ HW09_ch12_ex01.py
@@ -9,11 +9,21 @@
 ###############################################################################
 # Imports
 
-
 # Body
 def most_frequent(s):
-    ...
-
+    letter_and_count = {}
+    punctuation = " !.,"
+    for letter in s:
+        letter = letter.lower()
+        if letter not in punctuation:
+            letter_and_count.setdefault(letter, 0)
+            letter_and_count[letter] += 1
+    by_freq = []
+    for x,y in zip(letter_and_count.keys(),letter_and_count.values()):
+        by_freq.append((x,y))
+    sorted_by_freq = sorted(by_freq, key=lambda x: x[1], reverse=True)
+    for letter,count in sorted_by_freq:
+        print(letter)
 
 ###############################################################################
 def main():   # DO NOT CHANGE BELOW
